9b04ac0d1649460e70ca7b7b052bdc0915d3f1f7
John Paulett <john@paulett.org>
2016-07-25 21:36:52 +0000

    Basic django admin structure.

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('room', 'timestamp')
+admin.site.register(models.Message, MessageAdmin)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Room, RoomAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.User, UserAdmin)
